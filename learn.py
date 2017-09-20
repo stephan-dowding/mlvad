4a4cbd1c44724b6888e6a541f3a11adac7308bff
davidtan <davidtan@thoughtworks.com>
2017-09-21 06:24:26 +0800

    Remove data creation code and load data from data/*.npy instead

--- learn.py
+++ learn.py
@@ -1,178 +1,28 @@
+import io, random
 import keras
 import numpy
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
-import io
+from sklearn.model_selection import train_test_split
 
 chunk_size = 480
 
 model = Sequential()
 model.add(Dense(128, activation='tanh', input_dim=(chunk_size//2)))
-# model.add(Dropout(0.25))
-
+model.add(Dropout(0.25))
 for x in range(3):
     model.add(Dense(128, activation='tanh'))
-
 model.add(Dense(1, activation='sigmoid'))
-
 model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
+X = numpy.load('./data/samples.npy')
+y = numpy.load('./data/labels.npy')
 
-samples = []
-answer = []
-validation_samples = []
-validation_answer = []
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-data_file = io.FileIO('./sound/voiceC.pcm', mode='r')
-data = data_file.readall()
-data_file.close()
-
-end = len(data)
-
-# skip first chunk
-for x in range(chunk_size, end, chunk_size):
-    chunk = data[x:(x+chunk_size)]
-    sample = []
-    for s in range(0, chunk_size, 2):
-        p = int.from_bytes(chunk[s:(s+2)],byteorder='little', signed=True)
-        sample.append(p)
-
-    samples.append(sample)
-    answer.append(1)
-
-
-data_file = io.FileIO('./sound/quiet.pcm', mode='r')
-data = data_file.readall()
-data_file.close()
-
-# skip first chunk
-for x in range(chunk_size, end, chunk_size):
-    chunk = data[x:(x+chunk_size)]
-    sample = []
-    for s in range(0, chunk_size, 2):
-        p = int.from_bytes(chunk[s:(s+2)],byteorder='little', signed=True)
-        sample.append(p)
-
-    samples.append(sample)
-    answer.append(0)
-
-
-data_file = io.FileIO('./sound/voiceW.pcm', mode='r')
-data = data_file.readall()
-data_file.close()
-
-# skip first chunk
-for x in range(chunk_size, end, chunk_size):
-    chunk = data[x:(x+chunk_size)]
-    sample = []
-    for s in range(0, chunk_size, 2):
-        p = int.from_bytes(chunk[s:(s+2)],byteorder='little', signed=True)
-        sample.append(p)
-
-    samples.append(sample)
-    answer.append(1)
-
-
-data_file = io.FileIO('./sound/allhands.pcm', mode='r')
-data = data_file.readall()
-data_file.close()
-
-# skip first chunk
-for x in range(chunk_size, end, chunk_size):
-    chunk = data[x:(x+chunk_size)]
-    sample = []
-    for s in range(0, chunk_size, 2):
-        p = int.from_bytes(chunk[s:(s+2)],byteorder='little', signed=True)
-        sample.append(p)
-
-    samples.append(sample)
-    answer.append(0)
-
-data_file = io.FileIO('./sound/voiceR.pcm', mode='r')
-data = data_file.readall()
-data_file.close()
-
-# skip first chunk
-for x in range(chunk_size, end, chunk_size):
-    chunk = data[x:(x+chunk_size)]
-    sample = []
-    for s in range(0, chunk_size, 2):
-        p = int.from_bytes(chunk[s:(s+2)],byteorder='little', signed=True)
-        sample.append(p)
-
-    samples.append(sample)
-    answer.append(1)
-
-data_file = io.FileIO('./sound/not_voice.pcm', mode='r')
-data = data_file.readall()
-data_file.close()
-
-# skip first chunk
-for x in range(chunk_size, end, chunk_size):
-    chunk = data[x:(x+chunk_size)]
-    sample = []
-    for s in range(0, chunk_size, 2):
-        p = int.from_bytes(chunk[s:(s+2)],byteorder='little', signed=True)
-        sample.append(p)
-
-    samples.append(sample)
-    answer.append(0)
-
-data_file = io.FileIO('./sound/voiceS.pcm', mode='r')
-data = data_file.readall()
-data_file.close()
-
-# skip first chunk
-for x in range(chunk_size, end, chunk_size):
-    chunk = data[x:(x+chunk_size)]
-    sample = []
-    for s in range(0, chunk_size, 2):
-        p = int.from_bytes(chunk[s:(s+2)],byteorder='little', signed=True)
-        sample.append(p)
-
-    validation_samples.append(sample)
-    validation_answer.append(1)
-
-data_file = io.FileIO('./sound/quiet2.pcm', mode='r')
-data = data_file.readall()
-data_file.close()
-
-# skip first chunk
-for x in range(chunk_size, end, chunk_size):
-    chunk = data[x:(x+chunk_size)]
-    sample = []
-    for s in range(0, chunk_size, 2):
-        p = int.from_bytes(chunk[s:(s+2)],byteorder='little', signed=True)
-        sample.append(p)
-
-    validation_samples.append(sample)
-    validation_answer.append(0)
-
-data_file = io.FileIO('./sound/allhands2.pcm', mode='r')
-data = data_file.readall()
-data_file.close()
-
-# skip first chunk
-for x in range(chunk_size, end, chunk_size):
-    chunk = data[x:(x+chunk_size)]
-    sample = []
-    for s in range(0, chunk_size, 2):
-        p = int.from_bytes(chunk[s:(s+2)],byteorder='little', signed=True)
-        sample.append(p)
-
-    validation_samples.append(sample)
-    validation_answer.append(0)
-
-
-X = numpy.mat(samples)
-y = numpy.array(answer)
-
-val_X = numpy.mat(validation_samples)
-val_y = numpy.array(validation_answer)
-
-model.fit(X, y, epochs=50, batch_size=32, validation_data=(val_X, val_y))
+model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
 
 model.save("vad.h5")
 #print(model.to_yaml())
